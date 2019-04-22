143685082de7300562085094644b81ac31f12476
mokahoka <mayank.kathuria@1mg.com>
2019-04-22 17:49:02 +0530

    Adds server functionality

---
+++ server/dummyData.js
@@ -0,0 +1,39 @@
+// Dummy Data collections for mocking backend functionality
+
+module.exports = {
+	users: [
+		{
+			username: "mayank",
+			password: "1234"
+		},{
+			username: "priya",
+			password: "1234"
+		},{
+			username: "admin",
+			password: "password"
+		}],
+
+	products: [
+		{
+			name: "",
+			price: "",
+			image: "",
+		},{
+			name: "",
+			price: "",
+			image: "",
+		},{
+			name: "",
+			price: "",
+			image: "",
+		},{
+			name: "",
+			price: "",
+			image: "",
+		},{
+			name: "",
+			price: "",
+			image: "",
+		}],
+}
+
