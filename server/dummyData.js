cd377532344a08492d7c0426c876e20702950f39
mokahoka <mayank.kathuria@1mg.com>
2019-04-23 12:49:15 +0530

    Adds products detail

--- server/dummyData.js
+++ server/dummyData.js
@@ -15,25 +15,25 @@ module.exports = {
 
 	products: [
 		{
-			name: "",
-			price: "",
-			image: "",
+			name: "Himalaya Chyavanaprasha",
+			price: "₹171",
+			image: "https://res.cloudinary.com/du8msdgbj/images/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/v1527769632/kw7x7uk2mscy7hlecjng/himalaya-chyavanaprasha.jpg",
 		},{
-			name: "",
-			price: "",
-			image: "",
+			name: "Himalaya Herbals Purifying Neem Face Wash",
+			price: "₹108",
+			image: "https://res.cloudinary.com/du8msdgbj/images/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/v1527769424/nwghznf9cg6okfllkmro/himalaya-herbals-purifying-neem-face-wash.jpg",
 		},{
-			name: "",
-			price: "",
-			image: "",
+			name: "Himalaya Protective Sunscreen Lotion Spf 15",
+			price: "₹95",
+			image: "https://res.cloudinary.com/du8msdgbj/images/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/v1523871650/z4dgktdj1rtn4vzbe11t/himalaya-protective-sunscreen-lotion-spf-15.jpg",
 		},{
-			name: "",
-			price: "",
-			image: "",
+			name: "Himalaya Bleminor Anti-Blemish Cream",
+			price: "₹133",
+			image: "https://res.cloudinary.com/du8msdgbj/images/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/v1529325420/jp20wm8xwaomqtjdawof/himalaya-bleminor-anti-blemish-cream.jpg",
 		},{
-			name: "",
-			price: "",
-			image: "",
+			name: "Himalaya Wellness Pure Hands Sanitizer Lemon",
+			price: "₹158",
+			image: "https://res.cloudinary.com/du8msdgbj/images/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/v1529320254/yvalslyj7hlcwgtzdvpd/himalaya-wellness-pure-hands-sanitizer-lemon.jpg",
 		}],
 }
 
