b20aa6da220e2812d71f3af8b857375bd0296f3c
mokahoka <mayank.kathuria@1mg.com>
2019-04-26 17:26:07 +0530

    Adds product list item component

---
+++ src/component/productItems.js
@@ -0,0 +1,15 @@
+// Product Items component
+
+import React from 'react';
+
+export default class productItems extends React.Component{
+	
+	render(){
+		return (
+			<div className="product-items">
+				<h2>Browse Product Items</h2>
+			{/* product cards component */}
+			</div>
+			);
+	}
+}
