3867ce78ac262b4a2383c468cce9c81d74eed2c9
mokahoka <mayank.kathuria@1mg.com>
2019-04-26 11:29:34 +0530

    adds redux reducers module

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,1 +1,44 @@
 // Redux reducer module
+
+import  {combineReducers} from 'redux';
+import { ADD_USERNAME, REMOVE_USERNAME, INCREMENT, DECREMENT, REMOVE_ITEM };
+
+
+function userReducer(state = "", action){
+	switch action.type{
+		
+		case ADD_USERNAME:
+			return action.payload;
+
+		case REMOVE_USERNAME:
+			return { username: "" }
+
+		default:
+			return state
+	}
+}
+
+function cartReducer(state = [], action){
+	switch action.type{
+		
+		case INCREMENT:
+			const product = state.cart.length === 0 ? action.payload.item 
+							: state.cart.filter((val) => val.name !== action.payload.item.name ) 
+			product.quantity += 1;
+			return product;
+
+		case DECREMENT:
+			return { username: "" }
+
+		default:
+			return state
+	}
+}
+
+const mainReducer = combineReducers({
+	user: userReducer,
+	cart: cartReducer,
+})
+
+
+export default mainReducer;
