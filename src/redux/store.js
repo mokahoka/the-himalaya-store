a5cb4c4fb31851ee2ccef8f2794d3ab7e1ca6339
mokahoka <mayank.kathuria@1mg.com>
2019-04-26 11:29:52 +0530

    adds redux store module

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,13 @@
 // Redux Store module
+
+import { createStore } from 'redux';
+import mainReducer from './reducer.js';
+
+// initial state of store
+const initialState = {
+	username: "",
+	cart = [],
+}
+
+// Redux store
+const store = createStore(mainReducer, initialState);
