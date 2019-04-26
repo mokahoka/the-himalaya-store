0e7e9152e1364d130ae0ab6ffacd4ac804df9430
mokahoka <mayank.kathuria@1mg.com>
2019-04-26 11:25:40 +0530

    adds redux actions module

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,51 @@
 // Redux actions module
 
-// ACTIONS
+
+// USERS ACTIONS
+export const ADD_USERNAME = "ADD_USERNAME";
+export const REMOVE_USERNAME = "REMOVE_USERNAME";
+
+// CART ACTIONS
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const REMOVE_ITEM = "REMOVE_ITEM";
+
+// USER ACTION CREATORS
+
+export const addUsername = (username) => ({
+	type: ADD_USERNAME,
+	payload: {
+		username,
+	}
+})
+
+export const removeUsername = (username) => ({
+	type: REMOVE_USERNAME,
+	payload: {
+		username,
+	}
+})
 
 
-// ACTIONS CREATORS
+// CART ACTION CREATORS
+
+export const incrementItem = (item) => ({
+	type: INCREMENT,
+	payload: {
+		item,
+	}
+})
+
+export const decrementItem = (item) => ({
+	type: DECREMENT,
+	payload: {
+		item,
+	}
+})
+
+export const removeItem = (item) => ({
+	type: REMOVE_ITEM,
+	payload: {
+		item,
+	}
+})
