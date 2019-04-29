7e29d9b3058a08058472df375d5395f8e64cc1b8
mokahoka <mayank.kathuria@1mg.com>
2019-04-29 16:19:34 +0530

    Implements add to cart functionality

--- src/component/cart.js
+++ src/component/cart.js
@@ -1,19 +1,44 @@
 // Cart component
 
 import React from 'react'
+import ProductListItem from './productListItem.js';
+import Counter from './counter.js';
+import {connect} from 'react-redux';
+import { incrementItem, decrementItem } from '../redux/actions.js'
 
-export default class Cart extends React.Component{
+
+class Cart extends React.Component{
+
 	render(){
 		return (
 			<div className="cart">
 				<div className="cart-header">
 					<h2>Items Added to Cart</h2>
 				</div>
-				<div className="cart-items"
+				<div className="cart-items">
 					{/* Cart Items component */}
-					<button onClick="">To Checkout</button>
+					{ this.props.cart.map((product, i) => (
+						<div className="cart-item" key={i}>
+							<ProductListItem name={product.name} amount={product.price} />
+							{/* The counter changes the item's position in an array */}
+							<Counter quantity={product.amount} increment={ () => this.props.increment(product) } decrement={ () => this.props.decrement(product) } />
+						</div>)) }
+					<button onClick={() => this.props.history.push(`/order-summary`) }>To Checkout</button>
 				</div>
 			</div>
 			)
 	}
 }
+
+// this.props.history.push(`/order-summary`)
+
+const mapStateToProps = (state) => ({
+	cart: state.cart
+})
+
+const mapDispatchToProps = {
+	increment: incrementItem,
+	decrement: decrementItem,
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Cart);
