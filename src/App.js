42d9cd320d6be02c8c81b278e603da06eedb2e54
mokahoka <mayank.kathuria@1mg.com>
2019-04-29 16:17:41 +0530

    Links to order summary component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 import { Route } from 'react-router-dom'; 
 import LogInForm from './component/LogInForm.js';
 import SignUpForm from './component/signUpForm.js';
+import BrowseProducts from './component/browseProducts.js';
+import OrderSummary from './component/orderSummary.js';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path='/' component={SignUpForm} />
+        <Route exact path='/' component={BrowseProducts} />
         <Route path='/log-in' component={LogInForm} />
         <Route path='/sign-up' component={SignUpForm} />
-        {/* <Route path='/cart' component={Cart} */}
-      {/* <Route path='/order-summary' component={OrderSummary} */}
+        <Route path='/cart' component={BrowseProducts}/>
+      <Route path='/order-summary' component={ OrderSummary } />
       </div>
     );
   }
