78ef304b6b317486666a5f585bc68a433df26f33
mokahoka <mayank.kathuria@1mg.com>
2019-05-02 09:01:19 +0530

    Adds header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './component/header.js';
 import { Route } from 'react-router-dom'; 
 import LogInForm from './component/LogInForm.js';
 import SignUpForm from './component/signUpForm.js';
@@ -11,6 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Route exact path='/' component={BrowseProducts} />
         <Route path='/log-in' component={LogInForm} />
         <Route path='/sign-up' component={SignUpForm} />
