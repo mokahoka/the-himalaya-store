8dbe6f3b043c5ee106ca0395a25b88d86cec281f
mokahoka <mayank.kathuria@1mg.com>
2019-05-02 09:02:08 +0530

    Adds header component module

---
+++ src/component/header.js
@@ -0,0 +1,11 @@
+// Website header Component
+
+import React from 'react';
+
+const header = () => (
+	<div className="header">
+		<img src="../../himayala-logo.jpg" />
+	</div>
+	)
+
+export default header;
